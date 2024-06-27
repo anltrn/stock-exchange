404b529337aa30101117536f64b0f67ce47724a9
Anıl Turan <anil.trn45@gmail.com>
2024-06-27 22:33:52 +0300

    StockExchangeController CustomExceptionHandler

--- src/main/java/com/stock/exchange/controller/StockExchangeController.java
+++ src/main/java/com/stock/exchange/controller/StockExchangeController.java
@@ -1,9 +1,44 @@
 package com.stock.exchange.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.stock.exchange.entity.Stock;
+import com.stock.exchange.entity.StockExchange;
+import com.stock.exchange.service.StockExchangeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/stock-exchange")
 public class StockExchangeController {
+
+    @Autowired
+    private StockExchangeService stockExchangeService;
+    @DeleteMapping(value="/{name}")
+    public ResponseEntity<Void> deleteStockFromExchange(@PathVariable("name") String stockExchangeName,
+                                                  @RequestParam("stockName") String stockName) {
+        stockExchangeService.deleteStockFromExchange(stockExchangeName, stockName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping(value="/{name}")
+    public ResponseEntity<Void> addStockToExchange(@PathVariable("name") String stockExchangeName,
+                                                   @RequestParam("stockName") String stockName) {
+        stockExchangeService.addStockToExchange(stockExchangeName, stockName);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping(value="/{name}")
+    public ResponseEntity<List<Stock>> stockExchangeStocks(@PathVariable("name") String stockExchangeName){
+        List<Stock> stocks = stockExchangeService.getAllStocksByExchangeName(stockExchangeName);
+        return new ResponseEntity<>(stocks, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<StockExchange> createStockExchange(@RequestBody StockExchange stockExchange) {
+        StockExchange createdExchange = stockExchangeService.createStockExchange(stockExchange);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdExchange);
+    }
 }
